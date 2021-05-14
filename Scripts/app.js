d686f4c01b327033d9b8ebd74ba25a531c6f11f7
daviccarvalho <dcavalc2@my.centennialcollege.ca>
2021-05-14 16:43:07 -0300

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,22 @@
+// IIFE
+"use strict";
+
+(function(){
+
+    function Start(){
+        let myVariable = 10;
+console.log("My number is: " + myVariable);
+
+    /*for (let index = 0; index < myVariable; index++)
+        {
+        console.log("your number is " + index);*
+        }
+    console.log(index);*/
+
+}
+
+window.addEventListener("load", Start);
+
+})();
+
+
